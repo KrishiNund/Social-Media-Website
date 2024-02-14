35ba1370f9436a31ffead545f52a2d6d8c46c419
K_Nund <ON144@live.mdx.ac.uk>
2024-02-15 00:13:31 +0400

    Finalised Header and created feed
    -added feed and right sidebar
    -added three posts

--- main.js
+++ main.js
@@ -19,85 +19,239 @@ function goTo(path) {
   }
 }
 
-function loadPageContent(content) {
-  const app = document.getElementById("app");
-  app.innerHTML = createHeader() + content + createFooter() ;
+function loadPageContent(content,type) {
+  if (type == "homepage"){
+    const app = document.getElementById("app");
+    app.innerHTML = createHeader() + content;
+  }
 }
 
-function createHeader(){
-    return `<nav class="navigation_bar navbar navbar-expand-sm bg-warning navbar-dark fixed-top">
+function createHeader() {
+  return `<nav class="navigation_bar navbar navbar-expand-sm bg-warning navbar-dark fixed-top">
+  <div class="container-fluid">
+      <div class="d-flex flex-row align-items-center justify-content-between">
+          <div class="flex-grow-1">
+              <!--Logo and Title-->
+              <a class="navbar-brand ms-left" href="#">
+                  <img src="images/icons8-honkai-star-rail-logo-50.png" class="img-fluid" alt="Logo">
+                  <span class="navbar-text" style="color: #B8BBD6;">Trailblazers' Hangout</span>
+              </a>
+          </div>
 
-    <div class="container-fluid">
+          <div class="flex-grow-1">
+              <!--Search Bar-->
+              <div class="form search_bar">
+                  <i class="fa fa-search"></i>
+                  <input type="text" class="form-control form-input" placeholder="Search Anything...">
+              </div>
+          </div>
 
-        <div class="row align-items-center">
-            <div class="col-sm-4">
-                <!--Logo and Title-->
-                <a class="navbar-brand ms-left" href="#">
-                    <img src="images/icons8-honkai-star-rail-logo-40 (1).png" class="img-fluid" alt="Logo">
-                    <span class="navbar-text font" style="color: #424874;">Trailblazers' Hangout</span>
-                </a>
-            </div>
+          <!--Menu-->
+          <div class="d-flex align-items-center flex-grow-1">
+              <!--Sign Up-->
+              <div class="flex-grow-1">
+                  <button type="button" class="btn go_to_signup_page">Sign Up</button>
+              </div>
+              <!--login-->
+              <div class="flex-grow-1">
+                  <button type="button" class="btn go_to_login_page">Login</button>
+              </div>
+              <!--User profile-->
 
-            <div class="col-sm-4">
-                <!--Search Bar-->
-                <div class="form search_bar">
-                    <i class="fa fa-search"></i>
-                    <input type="text" class="form-control form-input" placeholder="Search Anything...">
-                </div>
-            </div>
+              <div class="dropdown user_profile_button flex-grow-1">
+                  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
+                     <img src="images/icons8-user-profile-48.png" alt="User Profile" class="rounded-circle">
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-end">
+                      <li><span class="dropdown-item-text">Status:Logged In/ Logged Out</span></li>
+                  </ul>
+              </div>
 
-            <div class="col-sm-4" style="display: flex;">
-                <!--Menu-->
-                <div>
-                    <button type="button" class="btn go_to_signup_page font">
-                        <!-- <img src="images/icons8-sign-up-40.png" alt="redirectToSignUpPageButton"> -->
-                        Sign Up
-                    </button>
-
-                </div>
-
-                <div>
-                    <button type="button" class="btn go_to_login_page font">
-                        <!-- <img src="images/icons8-sign-up-40.png" alt="redirectToSignUpPageButton"> -->
-                        Login
-                    </button>
-
-                </div>
-                
-                <div class="dropdown user_profile_button">
-                    <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
-                       <img src="images/icons8-user-profile-40.png" alt="User Profile" class="rounded-circle">
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><span class="dropdown-item-text">Status:Logged In/ Logged Out</span></li>
-                    </ul>
-                </div>
-
-                <div>
-                    <!--Settings button-->
-                    <button type="button" class="btn settings_button">
-                        <img src="images/icons8-settings-40 (1).png" alt="settings button" class="img-fluid">
-                    </button>
-                </div>
-            </div>
-
-        </div>
-   
-    </div>
-</nav>`
+              <!--Settings-->
+              <div class="flex-grow-1">
+                  <!--Settings button-->
+                  <button type="button" class="btn settings_button">
+                      <img src="images/icons8-settings-40.png" alt="settings button" class="img-fluid">
+                  </button>
+              </div>
+          </div>
+      </div>
+  </div>
+</nav>`;
 }
 
-function createFooter(){
-
-}
-
+function createFooter() {}
 
 function loadHomePage() {
-  const homePageContent = `<h2 style="text-align: center;color: black;">This is working!</h2>
-    <div width="500" height="500" style="display:flex">
-     <img src="images/nahidwin.jpg" alt="Gojo" width="500" height="500">
-     <img src="images/gigachad-cat-v0-mfrsimrgvi1c1.webp" alt="Giga cat" width="500" height="500">
-    </div>`;
+  const homePageContent = `<div class="container-fluid d-flex flex-row align-items-center justify-content-between">
+  <!--Main Content-->
+  <div>
+      <!--Feed menu-->
+      <div class="feed_options d-flex flex-row align-items-center justify-content-between rounded fixed-top">
+          <button type="button" class="btn">Following</button>
+          <button type="button" class="btn">Explore</button>
+          <button type="button" class="btn">Events</button>
+      </div>
 
-    loadPageContent(homePageContent);
+      <!--Posts-->
+      <div class="post_area">
+
+          <!--First Post-->
+          <div class="post">
+              <!--Post related buttons-->
+              <div class="user_info d-flex align-items-center">
+                  <img src="images/icons8-user-profile-48.png" class="user_profile img-fluid" alt="user profile">
+                  <span style="position: relative; left: 1.5em;">CatKittyCatCat</span>
+                  <button type="button" class="btn follow_button">Follow</button>
+                  <button type="button" class="btn">⋮</button>
+              </div>
+              
+              <!--Post Contents-->
+              <div class="post_content d-flex flex-column">
+                  <span><h5>Can I stop farming now?</h2></span>
+                  <img src="images/dan_heng_build.jpg" alt="dan_heng_build" class="img-fluid rounded" >
+                  <span style="color: #533A7B;"><h6>#danheng #buildreview</h6></span>
+              </div>
+  
+              <hr>
+
+              <!--Adding engagement buttons: like, dislike, comment-->
+              <div class="d-flex flex-row engagement_buttons">
+                  <button type="button" class="btn">
+                      <i class="fa fa-heart-o"></i>
+                  </button>
+
+                  <button type="button" class="btn">
+                      <i class="fa fa-thumbs-o-down"></i>
+                  </button>
+
+                  <button type="button" class="btn">
+                      <i class="fa fa-comment-o"></i>
+                  </button>
+              </div>
+          </div>
+
+          <!--Second Post-->
+          <div class="post">
+              <!--Post related buttons-->
+              <div class="user_info d-flex align-items-center">
+                  <img src="images/icons8-user-profile-48.png" class="user_profile img-fluid" alt="user profile">
+                  <span style="position: relative; left: 1.5em;">GingerCat</span>
+                  <button type="button" class="btn follow_button">Follow</button>
+                  <button type="button" class="btn">⋮</button>
+              </div>
+              
+              <!--Post Contents-->
+              <div class="post_content d-flex flex-column">
+                  <span><h5>Which banner do you recommend pulling on?</h2></span>
+                  <img src="images/banner.jpg" alt="dan_heng_build" class="img-fluid rounded" >
+                  <span style="color: #533A7B;"><h6>#banner #newupdate</h6></span>
+              </div>
+  
+              <hr>
+
+              <!--Adding engagement buttons: like, dislike, comment-->
+              <div class="d-flex flex-row engagement_buttons">
+                  <button type="button" class="btn">
+                      <i class="fa fa-heart-o"></i>
+                  </button>
+
+                  <button type="button" class="btn">
+                      <i class="fa fa-thumbs-o-down"></i>
+                  </button>
+
+                  <button type="button" class="btn">
+                      <i class="fa fa-comment-o"></i>
+                  </button>
+              </div>
+          </div>
+
+          <!--Third Post-->
+          <div class="post">
+              <!--Post related buttons-->
+              <div class="user_info d-flex align-items-center">
+                  <img src="images/icons8-user-profile-48.png" class="user_profile img-fluid" alt="user profile">
+                  <span style="position: relative; left: 1.5em;">GigaCat</span>
+                  <button type="button" class="btn follow_button">Follow</button>
+                  <button type="button" class="btn">⋮</button>
+              </div>
+              
+              <!--Post Contents-->
+              <div class="post_content d-flex flex-column">
+                  <span><h5>How to defeat this boss in less than 2 turns?</h2></span>
+                  <img src="images/boss_battle.jpg" alt="dan_heng_build" class="img-fluid rounded" >
+                  <span style="color: #533A7B;"><h6>#bossbattle #cocolia</h6></span>
+              </div>
+  
+              <hr>
+
+              <!--Adding engagement buttons: like, dislike, comment-->
+              <div class="d-flex flex-row engagement_buttons">
+                  <button type="button" class="btn">
+                      <i class="fa fa-heart-o"></i>
+                  </button>
+
+                  <button type="button" class="btn">
+                      <i class="fa fa-thumbs-o-down"></i>
+                  </button>
+
+                  <button type="button" class="btn">
+                      <i class="fa fa-comment-o"></i>
+                  </button>
+              </div>
+          </div>
+
+      </div>
+  </div>
+
+  <!--Right sidebar-->
+  <div class="d-flex flex-column position-fixed sidebar">
+
+      <div class="trending_hashtags text-center">
+          <div class="widget_header"><h4>Trending</h4></div>
+          <span>#PenaconyUpdate <hr></span>
+          <span>#Version2.0 <hr></span>
+          <span>#Firefly <hr></span>
+          <span>#HSR</span>
+      </div>
+
+      <div class="post_panel text-center">
+          <div class="widget_header"><h4>Post Now~~</h4></div>
+
+          <div class="d-flex flex-row post_options">
+              <button type="button" class="btn">
+                  <i class="fa fa-camera fa-2x"></i>
+              </button>
+
+              <button type="button" class="btn">
+                  <i class="fa fa-video-camera fa-2x"></i>
+              </button>
+
+              <button type="button" class="btn">
+                  <i class="fa fa-pencil-square-o fa-2x"></i>
+              </button>
+
+          </div>
+      </div>
+
+      <div class="contact_box text-center">
+          <div class="widget_header"><h4>Contact</h4></div>
+
+          <span>
+              <a href="#">About Us</a>
+              <br>
+              <a href="#">Terms of Use</a>
+              <br>
+              <a href="#">Privacy Policy</a>
+              <br>
+              Email Address: ON144@live.mdx.ac.uk 
+              <br>
+              Copyright &copy; 2024 Trailblazers' Hangout 
+          </span>
+      </div>
+
+  </div>
+</div>`;
+
+  loadPageContent(homePageContent,"homepage");
 }
