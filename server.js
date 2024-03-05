21b48212a52e4a928c17c23206e8c0c91e86abb3
K_Nund <ON144@live.mdx.ac.uk>
2024-03-05 22:44:30 +0400

    creating functions for getting users

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const path = require("path");
 const express = require("express");
 const mongodb = require("mongodb");
 const bodyParser = require("body-parser");
+const expressSession = require('express-session');
 
 //creating express application
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
+
+//configure express to use express-session
+app.use(
+  expressSession({
+    secret:'cst2120 secret',
+    cookie:{maxAge:6000},
+    resave:false,
+    saveUninitialized:true
+  })
+);
 
 //connection details
 const password = "M00934333+2023";
@@ -49,6 +60,53 @@ app.use("/", express.static(path.join(__dirname)));
 const database = client.db("Social_Media_Website");
 const collection = database.collection("Example_Users");
 
+//set up application to handle get requests
+// app.get('/M00934333/users', getUsers);
+
+async function getUsers(){
+  const query = {};
+
+  const results = await collection.find(query).toArray();
+
+  return results;
+ 
+}
+
+async function fetchData(){
+  const results = await getUsers();
+  console.log(results);
+
+  for (const result of results){
+    console.log(result.firstName);
+  }
+
+  console.log(results[0].firstName);
+}
+
+fetchData();
+
+
+// app.get('/M00934333/checklogin', checkLogin);
+// app.get('/M00934333/logout', logout);
+
+
+//set up application to handle post requests
+// app.post('/M00934333/login',login);
+// app.post('/M00934333/register',register);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //post request
 app.post("/M00934333", async (req, res) => {
   const userData = {
@@ -60,23 +118,22 @@ app.post("/M00934333", async (req, res) => {
 
   const result = await collection.insertOne(userData);
 
-  console.log(result);
-
-  res.send("Data Received successfully");
+  res.send(`Data Received successfully: ${JSON.stringify(result)}`);
 });
 
 
 //get request
-app.get("/M00934333", async (req, res) => {
-  const query = {};
+// app.get("/M00934333", async (req, res) => {
+//   const query = {};
 
-  const results = await collection.find(query).toArray();
+//   const results = await collection.find(query).toArray();
 
-  res.setHeader("Content-Type", "application/json");
+//   res.setHeader("Content-Type", "application/json");
 
-  res.send(JSON.stringify(results));
-});
+//   res.send(JSON.stringify(results));
+// });
 
+//starting server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
